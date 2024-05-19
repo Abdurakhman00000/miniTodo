f6d821480641d84f98dd2b0454a6901e74456740
Abdurakhman00000 <abdibaitovabdurahman@gmail.com>
2024-05-19 15:12:33 +0600

    commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (inputValue.trim() !== '') {
+      setTasks([...tasks, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Введите"
+        />
+        <button onClick={handleAddTask}>Добавить</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
